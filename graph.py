b1bed4e7415c16ad7b428eb6eb65ad9c95fe4416
apoudel20 <aayush.poudel21@gmail.com>
2021-08-06 05:10:25 -0400

    reflecting off walls and animation complete

--- graph.py
+++ graph.py
@@ -1,23 +1,81 @@
+# import ast # parse list from file
+
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# from mpl_toolkits import mplot3d
+
+# fig = plt.figure()
+# ax = plt.axes(projection='3d')
+# # ax.set_xlim([0,100])
+# # ax.set_ylim([0,100])
+# # ax.set_zlim([0,100])
+
+# for i in range(6):
+
+#     position_data = pd.read_csv("frames/output_"+str(i)+".csv")
+    
+#     x = position_data.iloc[:,0][::1000]    
+#     y = position_data.iloc[:,1][::1000]
+#     z = position_data.iloc[:,2][::1000]
+#     ax.scatter3D(x, y, z, c = z)
+
+# plt.show()
+
 import ast # parse list from file
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from mpl_toolkits import mplot3d
+
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.animation import FuncAnimation
+
+# from numba import vectorize
+
+frames_rendered = 16
+x,y,z = [],[],[]
+
+for i in range(frames_rendered):
+    position_data = pd.read_csv("frames/output_"+str(i)+".csv")
+
+    x.append(position_data.iloc[:,0][::1000])   
+    y.append(position_data.iloc[:,1][::1000])
+    z.append(position_data.iloc[:,2][::1000])
+
+
+def animate_vector(i):
+    title.set_text("Frame: " + str(i))
+    position_plots._offsets3d = (x[i], y[i], z[i])
+
 
 fig = plt.figure()
-ax = plt.axes(projection='3d')
-# ax.set_xlim([0,100])
-# ax.set_ylim([0,100])
-# ax.set_zlim([0,100])
+ax = fig.add_subplot(111,projection='3d')
 
-for i in range(6):
+ax.set_xlim([-15,15])
+ax.set_ylim([-5,15])
+ax.set_zlim([-15,15])
+title = ax.set_title('3D Test')
+def get_cube():   
+    phi = np.arange(1,10,2)*np.pi/4
+    Phi, Theta = np.meshgrid(phi, phi)
 
-    position_data = pd.read_csv("frames/output_"+str(i)+".csv")
-    
-    x = position_data.iloc[:,0][::1000]    
-    y = position_data.iloc[:,1][::1000]
-    z = position_data.iloc[:,2][::1000]
-    ax.scatter3D(x, y, z, c = z)
+    x = np.cos(Phi)*np.sin(Theta)
+    y = np.sin(Phi)*np.sin(Theta)
+    z = np.cos(Theta)/np.sqrt(2)
+    return x,y,z
+
+
+a = 25
+b = 10
+c = 25
+cube_x,cube_y,cube_z = get_cube()
+
+
+position_plots = ax.scatter3D(x[0],y[0],z[0])
+        
+myAnimation = FuncAnimation(fig, animate_vector, frames=np.arange(1,frames_rendered), blit = False, interval=1)
+ax.plot_surface(cube_x*a+1, cube_y*b+b/2, cube_z*c+1,alpha=0.1)
 
 plt.show()
+
--- particlePusher.cu
+++ particlePusher.cu
@@ -8,10 +8,21 @@
 #define GRID_CENTER_OFFSET .25
 #define GRID_SIZE 100
 
-
+__device__ float vectorFieldX(float real_x, float real_y, float real_z,float vel_x, float vel_y, float vel_z,int timesteps){
+    float new_vel_x = real_x/10;//pow(real_x,2) * real_y;
+    return new_vel_x;
+}
+__device__ float vectorFieldY(float real_x, float real_y, float real_z,float vel_x, float vel_y, float vel_z,int timesteps){
+    float new_vel_y = 1/(exp(pow(0.1*real_x,4)))*1/(exp(pow(0.1*real_z,4)));//1/sqrt(1+pow((real_x),2) * pow((real_z),2));
+    return new_vel_y;
+}
+__device__ float vectorFieldZ(float real_x, float real_y, float real_z,float vel_x, float vel_y, float vel_z,int timesteps){
+    float new_vel_z = real_z/10;//real_x * real_y * real_z;
+    return new_vel_z;
+}
 
 __global__ 
-void rayTrace(int N, float *real_x, float *real_y, float *real_z, float timesteps){//, curandState *state){
+void rayTrace(int N, float *real_x, float *real_y, float *real_z,float *vel_x, float *vel_y, float *vel_z, int timesteps){//, curandState *state){
     int rayIndex = blockIdx.x*blockDim.x + threadIdx.x;
 
     if(rayIndex < N){
@@ -19,8 +30,52 @@ void rayTrace(int N, float *real_x, float *real_y, float *real_z, float timestep
         real_y[rayIndex] = 0;
         real_z[rayIndex] = ((float)rayIndex)/N*.5*GRID_CENTER_OFFSET*GRID_SIZE * sinpi(60*((float)rayIndex)/N);
     }
+    float d_vel_x, d_vel_y, d_vel_z;
 
-    real_y[rayIndex] += timesteps;
+    d_vel_x = vectorFieldX(real_x[rayIndex], real_y[rayIndex], real_z[rayIndex], vel_x[rayIndex],vel_y[rayIndex],vel_z[rayIndex],timesteps);
+    d_vel_y = vectorFieldY(real_x[rayIndex], real_y[rayIndex], real_z[rayIndex], vel_x[rayIndex],vel_y[rayIndex],vel_z[rayIndex],timesteps);
+    d_vel_z = vectorFieldZ(real_x[rayIndex], real_y[rayIndex], real_z[rayIndex], vel_x[rayIndex],vel_y[rayIndex],vel_z[rayIndex],timesteps);    
+
+    float inter_vel_x, inter_vel_y, inter_vel_z; // intermediate values before calculating boundary conditions for reflection off walls
+
+    inter_vel_x = vel_x[rayIndex] + d_vel_x + real_x[rayIndex];
+    inter_vel_y = vel_y[rayIndex] + d_vel_y + real_y[rayIndex];
+    inter_vel_z = vel_z[rayIndex] + d_vel_z + real_z[rayIndex];
+
+    // boundary reflection conditions
+    vel_x[rayIndex] += d_vel_x;
+    if(inter_vel_x > GRID_SIZE/8){
+        real_x[rayIndex] = 2 * GRID_SIZE/8 - inter_vel_x;
+        vel_x[rayIndex] = -vel_x[rayIndex];
+    }else if(inter_vel_x < -GRID_SIZE/8){
+        real_x[rayIndex] = -GRID_SIZE/8 - inter_vel_x;
+        vel_x[rayIndex] = -vel_x[rayIndex];
+    }else{
+        real_x[rayIndex] += vel_x[rayIndex];
+    }
+
+    vel_y[rayIndex] += d_vel_y;
+    if(inter_vel_y > GRID_SIZE/10){
+        real_y[rayIndex] = 2 * GRID_SIZE/10 - inter_vel_y;
+        vel_y[rayIndex] = -vel_y[rayIndex];
+    }else if(inter_vel_y < 0){
+        real_y[rayIndex] = -inter_vel_y;
+        vel_y[rayIndex] = -vel_y[rayIndex];
+    }else{
+        real_y[rayIndex] += vel_y[rayIndex];
+    }
+
+    vel_z[rayIndex] += d_vel_z;
+    if(inter_vel_z > GRID_SIZE/8){
+        real_z[rayIndex] = 2 * GRID_SIZE/8 - inter_vel_z;
+        vel_z[rayIndex] = -vel_z[rayIndex];
+    }else if(inter_vel_z < -GRID_SIZE/8){
+        real_z[rayIndex] = -GRID_SIZE/8 - inter_vel_z;
+        vel_z[rayIndex] = -vel_z[rayIndex];
+    }else{
+        real_z[rayIndex] += vel_z[rayIndex];
+    }
+
 }
 
 int main(int argc, char *argv[]) 
@@ -36,27 +91,50 @@ int main(int argc, char *argv[])
     }
 
     
-    float *real_x, *real_y, *real_z,*dev_real_x, *dev_real_y, *dev_real_z;
-    
+    float *real_x, *real_y, *real_z,*dev_real_x, *dev_real_y, *dev_real_z; // R3 coordinates to be mapped onto the gridspace 
+    float *vel_x, *vel_y, *vel_z, *dev_vel_x, *dev_vel_y, *dev_vel_z; // current velocities for the particles
+
+    // allocate space for position arrays    
     real_x = (float*)malloc(N * sizeof(float));
     real_y = (float*)malloc(N * sizeof(float));
     real_z = (float*)malloc(N * sizeof(float));
     
+    // allocate space for velocity arrays
+    vel_x = (float*)malloc(N * sizeof(float));
+    vel_y = (float*)malloc(N * sizeof(float));
+    vel_z = (float*)malloc(N * sizeof(float));
+
+    // allocate device space for position arrays
     cudaMalloc(&dev_real_x, N * sizeof(float));
     cudaMalloc(&dev_real_y, N * sizeof(float));
     cudaMalloc(&dev_real_z, N * sizeof(float));
+
+    // allocate device space for velocity arrays
+    cudaMalloc(&dev_vel_x, N * sizeof(float));
+    cudaMalloc(&dev_vel_y, N * sizeof(float));
+    cudaMalloc(&dev_vel_z, N * sizeof(float));
     
+    // initialize arrays to 0
     cudaMemset(dev_real_x, 0, N * sizeof(float));
     cudaMemset(dev_real_y, 0, N * sizeof(float));
     cudaMemset(dev_real_z, 0, N * sizeof(float));
+    cudaMemset(dev_vel_x, 0, N * sizeof(float));
+    cudaMemset(dev_vel_y, 0, N * sizeof(float));
+    cudaMemset(dev_vel_z, 0, N * sizeof(float));
 
-    rayTrace<<<(N+192)/256,256>>>(N, dev_real_x, dev_real_y, dev_real_z, timesteps);//, devStates);
+    for(int t = 0; t < timesteps; t++){
+        rayTrace<<<(N+192)/256,256>>>(N, dev_real_x, dev_real_y, dev_real_z,dev_vel_x, dev_vel_y, dev_vel_z, t);//, devStates);
+        cudaDeviceSynchronize();
+    }
 
+    // return results to host
     cudaMemcpy(real_x, dev_real_x, N * sizeof(float), cudaMemcpyDeviceToHost);
     cudaMemcpy(real_y, dev_real_y, N * sizeof(float), cudaMemcpyDeviceToHost);
     cudaMemcpy(real_z, dev_real_z, N * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(vel_x, dev_vel_x, N * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(vel_y, dev_vel_y, N * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(vel_z, dev_vel_z, N * sizeof(float), cudaMemcpyDeviceToHost);
 
-    cudaDeviceSynchronize();
 
     std::ofstream outputWrite;
     outputWrite.open("frames/output_"+std::to_string(timesteps)+".csv");
@@ -68,9 +146,20 @@ int main(int argc, char *argv[])
 
     outputWrite.close();
 
+    cudaFree(dev_vel_z);
+    cudaFree(dev_vel_y);
+    cudaFree(dev_vel_x);    
     cudaFree(dev_real_z);
     cudaFree(dev_real_y);
     cudaFree(dev_real_x);
+
+    free(vel_z);
+    free(vel_y);
+    free(vel_x);
+    free(real_z);
+    free(real_y);
+    free(real_x);
+    
 
 
 }
